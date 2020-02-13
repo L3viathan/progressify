166364ff6d10e63a0fb5f1ed1b0dc09552abd710
L3viathan <git@l3vi.de>
2020-02-13 21:53:27 +0100

    Support printing during this thing

--- progressify.py
+++ progressify.py
@@ -19,17 +19,20 @@ def work(instances):
 
         while instances:
             with print_lock:
-                print(f"\r{C_KILL}{C_UP}"*last_instances, end="", flush=True)
+                # print(f"\r{C_KILL}{C_UP}"*last_instances, end="", flush=True)
                 num_instances = None
                 for num_instances, instance in enumerate(instances):
+                    print(f"\r{C_KILL}", end="")
                     instance.draw()
             if num_instances is None:
                 break
             num_instances += 1
+            print(C_UP*num_instances, end="", flush=True)
             last_instances = num_instances
             sleep(0.05)
         with print_lock:
-            print(f"\r{C_KILL}{C_UP}"*(last_instances+1), end="", flush=True)
+            print("\n"*(last_instances+1), end="", flush=True)
+            print(f"\r{C_KILL}{C_UP}"*(last_instances+2), end="", flush=True)
         subprocess.run(["tput", "cnorm"])
 
 
@@ -169,10 +172,11 @@ class ProgressBar:
         _, w = map(int, proc.stdout.decode().split())
         return w
 
-    # @staticmethod
-    # def print(*args, **kwargs):
-    #     print(C_KILL, end="", flush=True)
-    #     print(*args, **kwargs)
+    @staticmethod
+    def print(*args, **kwargs):
+        with print_lock:
+            builtins.print(C_KILL+"\r", end="", flush=True)
+            builtins.print(*args, **kwargs)
 
 
 def progressify(iterable_or_function=None, **kwargs):
@@ -229,28 +233,29 @@ if __name__ == '__main__':
     #         p.value = (i + 1) / 25
     #         sleep(0.02)
 
-    for i, im in zip(range(2), progressify(["Homer", "Marge", "Bart", "Lisa"])):
-        ProgressBar.instances[0].message = im
-        ProgressBar.instances[0].value = (i + 1) / 4
-        for j, jm in zip(
-            range(2),
-            progressify(["likes", "loves", "hates", "dislikes", "has", "makes", "wants"]),
-        ):
-            ProgressBar.instances[1].message = jm
-            ProgressBar.instances[1].value = (j + 1) / 7
-            for k, km in zip(range(3), progressify(["cheese", "wine", "you"])):
-                ProgressBar.instances[2].message = km
-                ProgressBar.instances[2].value = (k + 1) / 3
-                sleep(0.1)
+    # for i, im in zip(range(2), progressify(["Homer", "Marge", "Bart", "Lisa"])):
+    #     ProgressBar.instances[0].message = im
+    #     ProgressBar.instances[0].value = (i + 1) / 4
+    #     for j, jm in zip(
+    #         range(2),
+    #         progressify(["likes", "loves", "hates", "dislikes", "has", "makes", "wants"]),
+    #     ):
+    #         ProgressBar.instances[1].message = jm
+    #         ProgressBar.instances[1].value = (j + 1) / 7
+    #         for k, km in zip(range(3), progressify(["cheese", "wine", "you"])):
+    #             ProgressBar.instances[2].message = km
+    #             ProgressBar.instances[2].value = (k + 1) / 3
+    #             sleep(0.1)
 
-    # @progressify
-    # def test(*, progress_bar):
-    #     for _ in range(20):
-    #         sleep(0.1)
-    #     for i in range(20):
-    #         progress_bar.value = i/20
-    #         progress_bar.message = i
-    #         # progress_bar.print("hello from", i)
-    #         sleep(0.1)
+    @progressify
+    def test(*, progress_bar):
+        for _ in range(20):
+            sleep(0.1)
+        for i in range(20):
+            progress_bar.value = i/20
+            progress_bar.message = i
+            if i % 5 == 0:
+                progress_bar.print("hello from", i)
+            sleep(0.1)
 
-    # test()
+    test()
